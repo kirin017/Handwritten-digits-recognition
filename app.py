c83ec35c0597db4191f261482a125557b09c6e80
huynguyenhuu <huy.nguyenhuu.017@gmail.com>
2023-05-29 17:26:11 +0700

    up

--- app.py
+++ app.py
@@ -8,17 +8,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Define route for the prediction
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     # Retrieve the uploaded image from the form
-#     image = request.files['file-input']
-
-#     # Add your deep learning model prediction code here
-#     # Process the image and obtain the prediction result
-
-#     # Return the prediction result as a response
-#     return render_template('index.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
